086fe144b8f44bb1981fef18957cacd11a8df9bc
ABHISHEK KUMAR SINGH <asociton@outlook.com>
2018-12-06 18:20:38 +0530

    test 3.2

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     while($i < $_GET["num"]){
       if($i%$_GET["num"]==0){
         $flag  = 1;
-        echo "<h1>".$_GET["num"]." is not prime.</h1>"
+        echo "<h1>".$_GET["num"]." is not prime.</h1>";
       }
+      $i++;
     }
     if($flag == 0){
-      echo "<h1>".$_GET["num"]." is prime.</h1>"
+      echo "<h1>".$_GET["num"]." is prime.</h1>";
     }
   }
 ?>
