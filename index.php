5251290ce22b22f44d9b6fc347e106cfe023add6
ABHISHEK KUMAR SINGH <asociton@outlook.com>
2018-12-06 18:13:19 +0530

    test 3

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-echo "Hello ".$_GET["name"];
+  if(is_numeric($_GET["num"])){
+    $flag = 0;
+    $i = 1;
+    while($i < $_GET["num"]){
+      if($i%$_GET["num"]==0){
+        $flag  = 1;
+        echo "<h1>".$_GET["num"]." is not prime.</h1>"
+      }
+    }
+    if($flag == 0){
+      echo "<h1>".$_GET["num"]." is prime.</h1>"
+    }
+  }
 ?>
 
 <form>
-  <input name = "name" type = "text">
-  <input type = "submit" value = "Greet">
+  <input name = "num" type = "number">
+  <input type = "submit" value = "is prime?">
 </form>
